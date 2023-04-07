57af90787a5c13b59d7918d0d00ce550dc751138
matthewemeh <mcmekus10@gmail.com>
2023-04-07 16:36:28 +0100

    added alert system

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -6,3 +6,24 @@ export const togglePassword = (element: React.RefObject<HTMLInputElement>) => {
   const oldType: string = inputElement.type;
   inputElement.type = oldType === 'password' ? 'text' : 'password';
 };
+
+export const showAlert = (
+  msg: string,
+  duration = 4000,
+  bgColor = '#181818',
+  textColor = '#fff'
+) => {
+  const alert = document.querySelector('#alert') as HTMLDivElement;
+
+  // show alert only when alert box is initially hidden
+  if (alert.style.bottom === '-100px') {
+    alert.style.background = bgColor;
+    alert.style.color = textColor;
+    alert.innerHTML = msg;
+    alert.style.bottom = '0px';
+
+    setTimeout(() => {
+      alert.style.bottom = '-100px';
+    }, duration);
+  }
+};
