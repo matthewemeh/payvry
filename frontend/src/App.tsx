c8e79d2e5c6974d3244f73cf29824daf5719d780
matthewemeh <mcmekus10@gmail.com>
2023-04-09 13:05:43 +0100

    updated interfaces, separated Student and Vdndor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 
-import { HistoryData, User } from './interfaces';
+import { HistoryData, Student, Vendor } from './interfaces';
 
 import HomeStudent from './pages/student/Home';
 import LoginStudent from './pages/student/Login';
@@ -16,12 +16,15 @@ import ReceiveMoneyStudent from './pages/student/ReceiveMoney';
 import HomeVendor from './pages/vendor/Home';
 import LoginVendor from './pages/vendor/Login';
 import SignUpVendor from './pages/vendor/SignUp';
+import HistoryVendor from './pages/vendor/History';
+import ProfileVendor from './pages/vendor/Profile';
 import CreatePinVendor from './pages/vendor/CreatePin';
+import ReceiveMoneyVendor from './pages/vendor/ReceiveMoney';
 
 // this information should be fetched and stored using useState and
 // then made available throughout the application.
 // this is just dummy data
-const history: HistoryData[] = [
+const history1: HistoryData[] = [
   {
     id: 'D32SASFGD243DF',
     title: 'Mobile top-up',
@@ -55,16 +58,52 @@ const history: HistoryData[] = [
     description: 'service payment',
   },
 ];
+const history2: HistoryData[] = [
+  {
+    id: 'D32SASFGD243DF',
+    title: 'CLU200203-442',
+    transactionAmount: 4000,
+    transactionType: 'credit',
+    date: '2023-03-24T17:03:00Z',
+    description: 'money received',
+  },
+  {
+    id: 'ASFD32S3DFGD24',
+    title: 'Jide Cole',
+    transactionAmount: 100000,
+    transactionType: 'debit',
+    date: '2023-03-24T15:03:00Z',
+    description: 'withdrawal',
+  },
+  {
+    id: 'ASFD32S3DFGD25',
+    title: 'CLU200203-442',
+    transactionAmount: 1000,
+    transactionType: 'debit',
+    date: '2023-03-24T03:03:00Z',
+    description: 'refund',
+  },
+];
 
-const user: User = {
-  history,
+const user1: Student = {
+  history: history1,
   pin: '123456',
   name: 'Joseph',
   balance: 168260,
-  clientType: 'student',
   password: 'jiffy101#',
   phoneNumber: '09061281792',
   matricNumber: 'clu200203-442',
+};
+
+const user2: Vendor = {
+  history: history2,
+  pin: '123456',
+  name: 'Blosom',
+  balance: 168260,
+  username: 'Buds',
+  password: 'jiffy101#',
+  vendorName: 'Deli Buds',
+  phoneNumber: '09061281792',
 };
 
 const App = () => {
@@ -78,8 +117,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Landing />}></Route>
 
-        <Route path='/student/home' element={<HomeStudent user={user} />}></Route>
-        <Route path='/student/history' element={<HistoryStudent user={user} />}></Route>
+        <Route path='/student/home' element={<HomeStudent user={user1} />}></Route>
+        <Route path='/student/history' element={<HistoryStudent user={user1} />}></Route>
         <Route
           path='/student/login'
           element={<LoginStudent studentBaseUrl={baseURLs.student} />}
@@ -94,18 +133,24 @@ const App = () => {
         ></Route>
         <Route
           path='/student/profile'
-          element={<ProfileStudent user={user} studentBaseUrl={baseURLs.student} />}
+          element={<ProfileStudent user={user1} studentBaseUrl={baseURLs.student} />}
         ></Route>
         <Route path='/student/send-money' element={<SendMoneyStudent />}></Route>
         <Route path='/student/receive-money' element={<ReceiveMoneyStudent />}></Route>
 
-        <Route path='/vendor/home' element={<HomeVendor />}></Route>
+        <Route path='/vendor/home' element={<HomeVendor user={user2} />}></Route>
+        <Route path='/vendor/history' element={<HistoryVendor user={user2} />}></Route>
         <Route path='/vendor/login' element={<LoginVendor />}></Route>
         <Route
           path='/vendor/sign-up'
           element={<SignUpVendor vendorBaseUrl={baseURLs.vendor} />}
         ></Route>
         <Route path='/vendor/create-pin' element={<CreatePinVendor />}></Route>
+        <Route
+          path='/vendor/profile'
+          element={<ProfileVendor user={user2} vendorBaseUrl={baseURLs.vendor} />}
+        ></Route>
+        <Route path='/vendor/receive-money' element={<ReceiveMoneyVendor />}></Route>
       </Routes>
     </Router>
   );
