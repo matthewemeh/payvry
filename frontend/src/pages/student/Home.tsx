417138542f9578a862e16686aa0d178f925d5dba
matthewemeh <mcmekus10@gmail.com>
2023-04-13 20:02:07 +0100

    updated home

--- frontend/src/pages/student/Home.tsx
+++ frontend/src/pages/student/Home.tsx
@@ -8,7 +8,7 @@ import eyeImage from '../../assets/svgs/eye.svg';
 import userImage from '../../assets/svgs/user.svg';
 import chatImage from '../../assets/svgs/chat.svg';
 
-import { HistoryData, Student } from '../../interfaces';
+import { HistoryData, UserResponse } from '../../interfaces';
 import { showAlert, showInfo, togglePassword } from '../../utils';
 
 import HistoryPanel from '../../components/student/HistoryPanel';
@@ -22,9 +22,14 @@ const Home: React.FC<Props> = ({ studentBaseUrl }) => {
 
   const balanceRef = useRef<HTMLInputElement>(null);
 
-  const [balance, setBalance] = useState(0);
+  const [amount, setAmount] = useState('');
   const [fullName, setFullName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [matricNumber, setMatricNumber] = useState('');
   const [history, setHistory] = useState<HistoryData[]>([]);
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const [paymentMade, setPaymentMade] = useState(false);
 
   // componentDidMount
   useEffect(() => {
@@ -44,60 +49,51 @@ const Home: React.FC<Props> = ({ studentBaseUrl }) => {
     axios
       .post('/user', payload, generalInfoConfig)
       .then(res => {
-        const response: { studentTransaction: HistoryData[]; student: Student; message: string } =
-          res.data;
-        setHistory(response.studentTransaction);
-        setFullName(response.student.fullName);
-        setBalance(response.student.balance);
+        const response: UserResponse = res.data;
+        const { student, studentTransaction } = response;
+        const { fullName, phoneNumber, balance, matricNumber } = student;
+
+        balanceRef.current!.value = `C${balance.toLocaleString()}`;
+
+        setFullName(fullName);
+        setPhoneNumber(phoneNumber);
+        setMatricNumber(matricNumber);
+        setHistory(studentTransaction);
       })
       .catch((error: AxiosError) => showAlert({ msg: error.message }));
   }, []);
 
-  const formRef = useRef<HTMLFormElement>(null);
-  const fullNameRef = useRef<HTMLInputElement>(null);
-  const receiptFileRef = useRef<HTMLInputElement>(null);
-  const matricNumberRef = useRef<HTMLInputElement>(null);
-  const [paymentMade, setPaymentMade] = useState(false);
-
   const sendReceipt = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const fullName: string = fullNameRef.current!.value;
-    const matricNumber: string = matricNumberRef.current!.value;
-    const receiptFile: File | null =
-      receiptFileRef.current!.files && receiptFileRef.current!.files[0];
     const SERVICE_ID = process.env.REACT_APP_SERVICE_ID!;
     const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY!;
     const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID!;
 
-    if (fullName && matricNumber && receiptFile) {
-      emailjs
-        .sendForm(SERVICE_ID, TEMPLATE_ID, formRef.current!, PUBLIC_KEY)
-        .then(() => showAlert({ msg: 'Your receipt has been sent', zIndex: '5' }))
-        .catch((error: EmailJSResponseStatus) => {
-          if (error.status === 413) {
-            // maximum email size is 50kb
-            // most likely a large image might be the cause of this error
-            showAlert({
-              zIndex: '5',
-              msg: 'Upload receipt image with smaller file size',
-            });
-          } else if (error.status === 0) {
-            // internet connection issues
-            showAlert({
-              zIndex: '5',
-              msg: 'Receipt not sent, check your internet connection and try again',
-            });
-          } else {
-            showAlert({
-              zIndex: '5',
-              msg: 'An unexpected error has occured, please try again',
-            });
-          }
-        });
-    } else {
-      showAlert({ msg: 'Please fill in empty fields', zIndex: '5' });
-    }
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, formRef.current!, PUBLIC_KEY)
+      .then(() => showAlert({ msg: 'Your receipt has been sent', zIndex: '5' }))
+      .catch((error: EmailJSResponseStatus) => {
+        if (error.status === 413) {
+          // maximum email size is 50kb
+          // most likely a large image might be the cause of this error
+          showAlert({
+            zIndex: '5',
+            msg: 'Upload receipt image with smaller file size',
+          });
+        } else if (error.status === 0) {
+          // internet connection issues
+          showAlert({
+            zIndex: '5',
+            msg: 'Receipt not sent, check your internet connection and try again',
+          });
+        } else {
+          showAlert({
+            zIndex: '5',
+            msg: 'An unexpected error has occured, please try again',
+          });
+        }
+      });
   };
 
   return (
@@ -124,36 +120,51 @@ const Home: React.FC<Props> = ({ studentBaseUrl }) => {
         {paymentMade ? (
           <form onSubmit={sendReceipt} ref={formRef}>
             <h4 className='font-semibold text-[18px]'>Receipt details</h4>
-            <label htmlFor='fullName' className='text-left mt-5 block'>
+            <label htmlFor='fullName' className='text-left mt-5 hidden'>
               <span className='block mx-3'>Full name</span>
               <input
+                disabled
                 type='text'
                 id='fullName'
                 name='fullName'
-                ref={fullNameRef}
+                value={fullName}
                 className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
               />
             </label>
 
-            <label htmlFor='matricNumber' className='text-left mt-5 block'>
+            <label htmlFor='matricNumber' className='text-left mt-5 hidden'>
               <span className='block mx-3'>Matric number</span>
               <input
+                disabled
                 type='text'
                 id='matricNumber'
                 name='matricNumber'
-                ref={matricNumberRef}
+                value={matricNumber}
                 className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
               />
             </label>
 
-            <label htmlFor='receipt' className='text-left mt-5 block'>
-              <span className='block mx-3'>Upload Receipt image</span>
+            <label htmlFor='phoneNumber' className='text-left mt-5 hidden'>
+              <span className='block mx-3'>Phone number</span>
               <input
-                type='file'
-                id='receipt'
-                name='receipt'
-                accept='.jpg, .png'
-                ref={receiptFileRef}
+                disabled
+                type='text'
+                id='phoneNumber'
+                name='phoneNumber'
+                value={phoneNumber}
+                className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
+              />
+            </label>
+
+            <label htmlFor='amount' className='text-left mt-5 block'>
+              <span className='block mx-3'>Amount paid</span>
+              <input
+                required
+                type='text'
+                id='amount'
+                name='amount'
+                value={amount}
+                onChange={e => setAmount(e.target.value)}
                 className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
               />
             </label>
@@ -176,7 +187,7 @@ const Home: React.FC<Props> = ({ studentBaseUrl }) => {
               <span className='font-normal mb-[11px] inline-block'>Account Name:</span> Payvry
             </p>
             <p className='bg-[rgba(253,90,93,0.1)] text-carnation rounded-lg py-3 px-2'>
-              The payment description should be <strong>DEPOSIT TO PAYVRY ACCOUNT</strong>
+              The payment description should be <strong>PAYVRY - {matricNumber}</strong>
             </p>
             <p className='mt-[11px] font-normal'>
               Made payment already?{' '}
@@ -205,9 +216,9 @@ const Home: React.FC<Props> = ({ studentBaseUrl }) => {
 
         <input
           disabled
+          value='C0'
           type='password'
           ref={balanceRef}
-          value={`C${balance.toLocaleString()}`}
           className='text-center outline-0 font-semibold text-[34px] leading-[41px] max-w-full'
         />
       </div>
