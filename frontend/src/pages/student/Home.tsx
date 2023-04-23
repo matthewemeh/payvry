dc4f0e586adc26eb717827aec1011af6286454a1
matthewemeh <mcmekus10@gmail.com>
2023-04-23 20:51:20 +0100

    start paystack integration

--- frontend/src/pages/student/Home.tsx
+++ frontend/src/pages/student/Home.tsx
@@ -2,17 +2,18 @@ import Cookies from 'js-cookie';
 import { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios, { AxiosRequestConfig, AxiosError } from 'axios';
-import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 import eyeImage from '../../assets/svgs/eye.svg';
 import userImage from '../../assets/svgs/user.svg';
 import chatImage from '../../assets/svgs/chat.svg';
 import eyeSlashImage from '../../assets/svgs/eye-slash.svg';
 
-import { StudentHistoryData, StudentResponse } from '../../interfaces';
 import { showAlert, showInfo, togglePassword } from '../../utils';
+import { StudentHistoryData, StudentResponse } from '../../interfaces';
 
 import HistoryPanel from '../../components/student/HistoryPanel';
+
+const PaystackPop = require('@paystack/inline-js');
 
 const Home = () => {
   const navigate = useNavigate();
@@ -20,14 +21,10 @@ const Home = () => {
   const balanceRef = useRef<HTMLInputElement>(null);
   const [balanceHidden, setBalanceHidden] = useState(true);
 
+  const [email, setEmail] = useState('');
   const [amount, setAmount] = useState('');
   const [fullName, setFullName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [matricNumber, setMatricNumber] = useState('');
   const [history, setHistory] = useState<StudentHistoryData[]>([]);
-
-  const formRef = useRef<HTMLFormElement>(null);
-  const [paymentMade, setPaymentMade] = useState(false);
 
   // componentDidMount
   useEffect(() => {
@@ -49,48 +46,25 @@ const Home = () => {
       .then(res => {
         const response: StudentResponse = res.data;
         const { student, studentTransaction } = response;
-        const { fullName, phoneNumber, balance, matricNumber } = student;
+        const { fullName, balance } = student;
 
         balanceRef.current!.value = `C${balance.toLocaleString()}`;
 
         setFullName(fullName);
-        setPhoneNumber(phoneNumber);
-        setMatricNumber(matricNumber);
         setHistory(studentTransaction);
       })
       .catch((error: AxiosError) => showAlert({ msg: error.message }));
   }, []);
 
-  const sendReceipt = (e: React.FormEvent<HTMLFormElement>) => {
+  const payWithPaystack = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const SERVICE_ID = process.env.REACT_APP_SERVICE_ID!;
-    const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY!;
-    const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID!;
-
-    emailjs
-      .sendForm(SERVICE_ID, TEMPLATE_ID, formRef.current!, PUBLIC_KEY)
-      .then(() =>
-        showAlert({
-          zIndex: '5',
-          duration: 5000,
-          msg: 'Payment verification in progress. Your account will be credited once payment is verified',
-        })
-      )
-      .catch((error: EmailJSResponseStatus) => {
-        if (error.status === 0) {
-          // internet connection issues
-          showAlert({
-            zIndex: '5',
-            msg: 'Receipt not sent, check your internet connection and try again',
-          });
-        } else {
-          showAlert({
-            zIndex: '5',
-            msg: 'An unexpected error has occured, please try again',
-          });
-        }
-      });
+    const paystack = new PaystackPop();
+    paystack.newTransaction({
+      key: 'pk_test_f10a3be05c1ae77ea713135351d4508fedffd92a',
+      amount,
+      email,
+    });
   };
 
   return (
@@ -113,88 +87,42 @@ const Home = () => {
         </Link>
       </header>
 
-      <div className='info-bubble pay-modal text-center hidden bg-white w-[370px] fixed z-[1] p-[30px] rounded-[30px] border-[1px] border-alto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        {paymentMade ? (
-          <form onSubmit={sendReceipt} ref={formRef}>
-            <h4 className='font-semibold text-[18px]'>Receipt details</h4>
-            <label htmlFor='fullName' className='text-left mt-5 hidden'>
-              <span className='block mx-3'>Full name</span>
-              <input
-                type='text'
-                id='fullName'
-                name='fullName'
-                value={fullName}
-                className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
-              />
-            </label>
+      <form
+        onSubmit={payWithPaystack}
+        className='info-bubble pay-modal text-center hidden bg-white w-[370px] fixed z-[1] p-[30px] rounded-[30px] border-[1px] border-alto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+      >
+        <h4 className='font-semibold text-[18px]'>Receipt details</h4>
 
-            <label htmlFor='matricNumber' className='text-left mt-5 hidden'>
-              <span className='block mx-3'>Matric number</span>
-              <input
-                type='text'
-                id='matricNumber'
-                name='matricNumber'
-                value={matricNumber}
-                className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
-              />
-            </label>
+        <label htmlFor='email' className='text-left mt-5 block'>
+          <span className='block mx-3'>Email</span>
+          <input
+            required
+            type='email'
+            id='email'
+            name='email'
+            onChange={e => setEmail(e.target.value)}
+            className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
+          />
+        </label>
 
-            <label htmlFor='phoneNumber' className='text-left mt-5 hidden'>
-              <span className='block mx-3'>Phone number</span>
-              <input
-                type='text'
-                id='phoneNumber'
-                name='phoneNumber'
-                value={phoneNumber}
-                className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
-              />
-            </label>
+        <label htmlFor='amount' className='text-left mt-5 block'>
+          <span className='block mx-3'>Amount paid</span>
+          <input
+            required
+            type='text'
+            id='amount'
+            name='amount'
+            onChange={e => setAmount(e.target.value)}
+            className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
+          />
+        </label>
 
-            <label htmlFor='amount' className='text-left mt-5 block'>
-              <span className='block mx-3'>Amount paid</span>
-              <input
-                required
-                type='text'
-                id='amount'
-                name='amount'
-                value={amount}
-                onChange={e => setAmount(e.target.value)}
-                className='placeholder:text-mine-shaft bg-grey-200 w-full rounded-[100px] py-[10px] px-5 mt-1'
-              />
-            </label>
-
-            <input
-              type='submit'
-              value='Send Receipt'
-              className='bg-mine-shaft text-white w-full py-[15px] rounded-[100px] mt-[22px] font-medium text-[15px] leading-[18px] tracking-[0.06em] cursor-pointer'
-            />
-          </form>
-        ) : (
-          <>
-            <h3 className='font-semibold text-[20px] leading-[26px]'>
-              <span className='font-normal mb-[11px] inline-block'>Account Number:</span> 8252546650
-            </h3>
-            <p className='mt-[11px] font-medium text-[16px] leading-7 text-[rgba(0,0,0,0.5)]'>
-              Bank: Moniepoint
-            </p>
-            <p className='mt-[11px] font-semibold text-[20px] leading-[26px]'>
-              <span className='font-normal mb-[11px] inline-block'>Account Name:</span> Payvry
-            </p>
-            <p className='bg-[rgba(253,90,93,0.1)] text-carnation rounded-lg py-3 px-2'>
-              The payment description should be <strong>PAYVRY - {matricNumber}</strong>
-            </p>
-            <p className='mt-[11px] font-normal'>
-              Made payment already?{' '}
-              <span
-                className='font-semibold cursor-pointer underline'
-                onClick={() => setPaymentMade(true)}
-              >
-                Click here
-              </span>
-            </p>
-          </>
-        )}
-      </div>
+        <input
+          type='submit'
+          value='Send Receipt'
+          className='bg-mine-shaft text-white w-full py-[15px] rounded-[100px] mt-[22px] font-medium text-[15px] leading-[18px] tracking-[0.06em] cursor-pointer'
+        />
+      </form>
 
       <div className='flex flex-col items-center gap-y-5 mt-[10px]'>
         <p className='font-medium text-[16px] leading-[27px] text-[rgba(0,0,0,0.5)]'>
@@ -222,10 +150,7 @@ const Home = () => {
 
       <div className='text-mine-shaft font-semibold text-[14px] leading-[17px] flex justify-between mt-[30px]'>
         <button
-          onClick={() => {
-            setPaymentMade(false);
-            showInfo({ classTarget: '.pay-modal' });
-          }}
+          onClick={() => showInfo({ classTarget: '.pay-modal' })}
           className='py-[14px] px-5 bg-[rgba(190,161,161,0.2)] rounded-[30px]'
         >
           Add money
